57d5f69b40a95f9b1e8050e5f6cac4d861231415
Varun Bajpai <your_email@gmail.com>
2026-06-23 21:58:34 +0530

    Change axios

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,19 @@
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 dotenv.config();
-import app from "./app.js"
-import {connectToDatabase} from "./db/Database_Connection.js"
+import app from './app.js';
+import { connectToDatabase } from './db/Database_Connection.js';
 
-connectToDatabase().then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log(`🚀 Server is running on port ${process.env.PORT_NO}`);
-    })
-}).catch(error=>{
-    console.error("Failed to connect to database. Server not started.", error); 
-})
+console.log("DB_KEY exists:", !!process.env.DB_KEY);
+console.log("DATABASE_NAME:", process.env.DATABASE_NAME);
+console.log("PORT:", process.env.PORT);
+
+connectToDatabase()
+  .then(() => {
+    console.log("DB Connected")
+    app.listen(process.env.PORT, () => {
+      console.log(`🚀 Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to database. Server not started.', error);
+  });
