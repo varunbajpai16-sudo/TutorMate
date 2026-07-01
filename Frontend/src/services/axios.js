cc18b71c83783c744c7de1ac02239667b344d911
Varun Bajpai <your_email@gmail.com>
2026-07-01 15:34:00 +0530

    changes

--- Frontend/src/services/axios.js
+++ Frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "https://tutormate-pzpe.onrender.com/api/v1/",
   withCredentials: true,
 });
 
